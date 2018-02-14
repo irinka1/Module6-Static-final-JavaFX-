7cb53bb8c3aabdd6fb3ff0d41ac8552656e1da20
Ирина <irenmast@gmail.com>
2018-02-14 14:41:59 +0200

    Update Star.java

--- src/Star.java
+++ src/Star.java
@@ -1,24 +1,49 @@
-import javafx.scene.layout.Pane;
+package Star;
+import javafx.geometry.Point2D;
+import javafx.scene.shape.Line;
+
 
 public class Star {
+    private int yCoordinate;
+    private int xCoordinate;
+    private int radius;
+    private double insideRadius;
+    private int countAngle;
 
-    private Point p1, p2;
-    private javafx.scene.shape.Line line;
-
-    public Star(Point p1, Point p2) {
-        this.p1 = p1;
-        this.p2 = p2;
+    public Star(int xCoordinate, int yCoordinate, int radius, int countAngle) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = Main.HEIGHT_SCREEN - yCoordinate;
+        this.radius = radius;
+        this.insideRadius = radius / 2.63;
+        this.countAngle = countAngle;
     }
 
-    public javafx.scene.shape.Line show(Pane root1) {
-        if (line == null) {
-            line = new javafx.scene.shape.Line();
-            root1.getChildren().add(line);
+    public Line[] drawStar() {
+        Line[] lineStar = new Line[countAngle * 2];
+        Point2D[] point = new Point2D[countAngle * 2];
+        double startAngleRad = Math.toRadians(-18);
+        double deltaAngleRad = Math.PI / countAngle;
+        for (int i = 0; i < countAngle * 2; i++) {
+            double angleRad = startAngleRad + i * deltaAngleRad;
+            double relX = Math.cos(angleRad);
+            double relY = Math.sin(angleRad);
+            if (i % 2 == 0) {
+                relX = relX * radius;
+                relY = relY * radius;
+            } else {
+                relX = relX * insideRadius;
+                relY = relY * insideRadius;
+            }
+            point[i] = new Point2D(xCoordinate + relX, yCoordinate + relY);
         }
-        line.setStartX(p1.getX());
-        line.setStartY(p1.getY());
-        line.setEndX(p2.getX());
-        line.setEndY(p2.getY());
-        return line;
+        // записываем координаты в линии
+        for (int i = 0; i < lineStar.length; i++) {
+            if (i == lineStar.length - 1) {
+                lineStar[i] = new Line(point[i].getX(), point[i].getY(), point[0].getX(), point[0].getY());
+            } else {
+                lineStar[i] = new Line(point[i].getX(), point[i].getY(), point[i + 1].getX(), point[i + 1].getY());
+            }
+        }
+        return lineStar;
     }
 }
